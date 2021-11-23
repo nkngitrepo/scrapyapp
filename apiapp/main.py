2f969b01d26d8d1dc0bc37a5812f5246fd34a7ca
nkngitrepo <93523500+nkngitrepo@users.noreply.github.com>
2021-11-23 11:53:25 +0530

    Add files via upload

--- apiapp/main.py
+++ apiapp/main.py
@@ -1,10 +1,14 @@
 from typing import Optional
-
+from pydantic import BaseModel
 from fastapi import FastAPI
 from databases import Database
 import sys
 
 database = Database("postgresql://admin:admin@db/admin")
+
+class CBOL(BaseModel):
+    name: str
+    description: Optional[str] = None
 
 app = FastAPI()
 
@@ -18,7 +22,16 @@ async def database_connect():
 @app.on_event("shutdown")
 async def database_disconnect():
     await database.disconnect()
-    
+
+@app.post("/CBOL/")
+async def insert_item(cbol: CBOL):
+    d = cbol.dict()
+    if not cbol.name:
+        return {"Error":"Mandatory fields are missing"}
+    query = "insert into container_bol_requests(cid) values (:cid)"
+    await database.execute_many(query=query,values=[{"cid":cbol.name.strip()}])
+    return d
+
 @app.get("/")
 async def read_root():
     query = "select * from container_bol_requests"
@@ -33,6 +46,8 @@ async def read_item(container_id: str):
     try:
         query = "select type from req_type where id='"+container_id+"';"
         results = await database.fetch_all(query=query)
+        if not results:
+            return {'ERROR':container_id+" seems invalid"}
         
         res =results
         if res[0]['type'].strip() == 'CONT':
