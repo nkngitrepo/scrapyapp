fdcd5a5c03a6d5c0da5d0233fb9b1797fd773478
nkngitrepo <93523500+nkngitrepo@users.noreply.github.com>
2021-11-23 18:33:01 +0530

    Add files via upload

--- app/scrap.py
+++ app/scrap.py
@@ -5,7 +5,6 @@ from scrapy.settings import default_settings
 import logging
 from scrapy.utils.project import get_project_settings
 from datetime import timedelta, datetime
-from prefect.schedules import IntervalSchedule
 from twisted.internet import reactor
 import config
 import psycopg2
@@ -70,7 +69,7 @@ class BilloflandingSpider(scrapy.Spider):
             for tr in th.xpath('tr'):
                 for td in tr.xpath('td'):
                     t = td.xpath('text()').get()
-                    s.append(t.strip())
+                    s.append(t.strip() if t else '')
                 
         bcd = dict(zip(bhd,s))
 
@@ -80,15 +79,14 @@ class BilloflandingSpider(scrapy.Spider):
         
         for i in range(1,num_cont+1):
             s = []
-            hd = ['type','finalpod','shippedto','pcd']
             hdr_xpath = config.bol_cont_hdr_ptrn%(i)
             for th in response.xpath(hdr_xpath):
-                
                 for tr in th.xpath('tr'):
-                    
                     for td in tr.xpath('td'):
                         t = td.xpath('text()').get() #extract_first()
-                        s.append(t.strip())
+                        s.append(t.strip() if t else '')
+
+            hd = ['type','finalpod','shippedto','pcd'] if len(s) == 4 else ['type','finalpod','finalpodeta','shippedto','pcd','etablank']
                     
             cd = dict(zip(hd,s))
 
@@ -107,13 +105,18 @@ class BilloflandingSpider(scrapy.Spider):
                 cmd[index]=r
 
             if not bol:
-                ins_query = """insert into container (cid, type, finalpod, shippedto, pcd, movement) values (%s,%s,%s,%s,%s,%s) on conflict (cid) do update set
-                            type = %s, finalpod = %s, shippedto = %s, pcd = %s, movement = %s;"""
+                ins_query = """insert into container (cid, type, finalpod, finalpodeta, shippedto, pcd, movement) values (%s,%s,%s,%s,%s,%s,%s) on conflict (cid) do update set
+                            type = %s, finalpod = %s, finalpodeta = %s, shippedto = %s, pcd = %s, movement = %s;"""
+                try:
+                    cur.execute(ins_query,(self.bofl,cd['type'],cd['finalpod'],cd.get('finalpodeta',''),cd['shippedto'],cd['pcd'],json.dumps(cmd),cd['type'],cd['finalpod'],cd.get('finalpodeta',''),cd['shippedto'],cd['pcd'],json.dumps(cmd)))
+                    #con.commit()
+                    cur.execute("insert into req_type (id, type) values (%s,%s) on conflict(id) do nothing",(self.bofl,"CONT"))
+                    con.commit()
+                except Exception as e:
+                    print ("Failed to persist container details to db",file=sys.stderr)
+                    print (str(e),file=sys.stderr)
+                    break
                 
-                cur.execute(ins_query,(self.bofl,cd['type'],cd['finalpod'],cd['shippedto'],cd['pcd'],json.dumps(cmd),cd['type'],cd['finalpod'],cd['shippedto'],cd['pcd'],json.dumps(cmd)))
-                #con.commit()
-                cur.execute("insert into req_type (id, type) values (%s,%s) on conflict(id) do nothing",(self.bofl,"CONT"))
-                con.commit()
             else:
                 cont_name = response.xpath(config.bol_con_name_ptrn%(i)).extract_first().split(':')[-1].strip()
                 td = {}
@@ -125,11 +128,15 @@ class BilloflandingSpider(scrapy.Spider):
         if bol:
             ins_query = """insert into bol (bid, depdate, vessel, pol, pod, transhipment, pcd, containers) values (%s,%s,%s,%s,%s,%s,%s,%s) on conflict (bid) do update set
                         depdate = %s, vessel = %s, pol = %s, pod = %s, transhipment = %s, pcd = %s, containers = %s;"""
-            cur.execute(ins_query,(self.bofl,bcd['Departure_Date'],bcd['Vessel'],bcd['pol'],bcd['pod'],bcd['transhipment'],bcd['pcd'],json.dumps(bcd),bcd['Departure_Date'],bcd['Vessel'],bcd['pol'],bcd['pod'],bcd['transhipment'],bcd['pcd'],json.dumps(bcd)))
-            #con.commit()
-            cur.execute("insert into req_type (id, type) values (%s,%s) on conflict(id) do nothing;",(self.bofl,"BOL"))
-            con.commit()
-            
+            try:
+                cur.execute(ins_query,(self.bofl,bcd['Departure_Date'],bcd['Vessel'],bcd['pol'],bcd['pod'],bcd['transhipment'],bcd['pcd'],json.dumps(bcd),bcd['Departure_Date'],bcd['Vessel'],bcd['pol'],bcd['pod'],bcd['transhipment'],bcd['pcd'],json.dumps(bcd)))
+                #con.commit()
+                cur.execute("insert into req_type (id, type) values (%s,%s) on conflict(id) do nothing;",(self.bofl,"BOL"))
+                con.commit()
+            except Exception as e:
+                print ("Failed to persist bol details to db",file=sys.stderr)
+                print (str(e),file=sys.stderr)
+
         con.close()
         logger.info ("done")
 
